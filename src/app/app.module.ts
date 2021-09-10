b0e486a35f3fff1e1fed2bd3afbb1335ae514f56
Carlos Josue <carlosjosuemn98@gmail.com>
2021-09-09 19:25:12 -0500

    app finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LayoutComponent],
+  imports: [BrowserModule, RouterModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/components/breadcrum/breadcrum.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreadcrumComponent } from './breadcrum.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [BreadcrumComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [BreadcrumComponent],
+})
+export class BreadcrumModule {}
