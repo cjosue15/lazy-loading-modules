53290fc2f7b67d8483ba26a2ef58057d41a0a992
Carlos Josue <carlosjosuemn98@gmail.com>
2021-09-22 23:22:39 -0500

    add components in app module and delete others

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { LayoutComponent } from './shared/layout/layout.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
+import { LocationComponent } from './location/location.component';
+import { HomeComponent } from './home/home.component';
+import { BreadcrumComponent } from './shared/components/breadcrum/breadcrum.component';
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent],
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    LocationComponent,
+    HomeComponent,
+    BreadcrumComponent,
+  ],
   imports: [BrowserModule, RouterModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
